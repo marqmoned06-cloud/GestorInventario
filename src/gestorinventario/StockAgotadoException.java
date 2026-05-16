af30bccd48959df8abc3f59abbd06c24345ce2b9
Emmanuel Marquez <marqmoned06@gmail.com>
2026-05-16 14:21:13 +0200

    [fix] añade método tieneStock en Item

--- src/gestorinventario/StockAgotadoException.java
+++ src/gestorinventario/StockAgotadoException.java
@@ -8,12 +8,36 @@ package gestorinventario;
  */
 public class StockAgotadoException extends Exception {
 
+    /** Código de error asociado a la excepción. */
+    private int codigoError;
+
     /**
      * Constructor de la excepción.
+     *
+     * @param mensaje    Mensaje descriptivo del error.
+     * @param codigoError Código numérico del error.
+     */
+    public StockAgotadoException(String mensaje, int codigoError) {
+        super(mensaje);
+        this.codigoError = codigoError;
+    }
+
+    /**
+     * Constructor simplificado sin código de error.
      *
      * @param mensaje Mensaje descriptivo del error.
      */
     public StockAgotadoException(String mensaje) {
         super(mensaje);
+        this.codigoError = 0;
+    }
+
+    /**
+     * Devuelve el código de error.
+     *
+     * @return El código de error.
+     */
+    public int getCodigoError() {
+        return codigoError;
     }
 }
